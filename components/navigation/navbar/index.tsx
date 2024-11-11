c7d7da933b80c9a7f1d8fcf79bf654843c5d9a96
s-kvng <kvngnathan8420@gmail.com>
2024-11-11 03:22:00 +0000

    add theme toggler

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
+import Theme from "./Theme-toggler";
+
 const Navbar = () => {
   return (
     <nav className=" flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12 ">
@@ -21,7 +23,7 @@ const Navbar = () => {
       <p>Global seach</p>
 
       <div className="flex-between gap-5">
-        Theme
+        <Theme />
       </div>
     </nav>
   );
