15cd984adc717d3018221c935014b908f59ab435
s-kvng <kvngnathan8420@gmail.com>
2024-12-10 16:49:02 +0000

    create home page template

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,29 +1,30 @@
-import { auth, signOut } from "@/auth";
+import Link from "next/link";
+
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
 
 const Home = async () => {
-  const session = await auth();
-  console.log(session);
   return (
     <>
-      <div className="p-4 text-xl text-violet-600">
-        Welcome to the world of Nextjs
-      </div>
+      <section className=" flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center ">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
 
-      <div className="p-4 font-space-grotesk text-xl text-violet-600">
-        Welcome to the world of Nextjs
+        <Button
+          asChild
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-12">Local Search</section>
+      home filter
+      <div className=" mt-10 flex w-full flex-col gap-6">
+        <p>Question 1</p>
+        <p>Question 1</p>
+        <p>Question 1</p>
+        <p>Question 1</p>
+        <p>Question 1</p>
       </div>
-
-      <form
-        className="pt-10"
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Logout</Button>
-      </form>
     </>
   );
 };
