d1a96d00c256cc385958f5ed9c395b67ea865b67
s-kvng <kvngnathan8420@gmail.com>
2024-12-13 04:27:32 +0000

    add local search bar 🔍
    * build local search bar as its own component ✔️

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,44 @@
 import Link from "next/link";
 
+import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
 
-const Home = async () => {
+const questions = [
+  { _id : "1", title :"I want to learn react ", description : "How do i go about learning ", tags: [
+    {_id : "1", name : "React"},
+    {_id : "2", name :"Javascript"}
+  ], author : { _id : "1", name: "John doe"},
+upvotes : 10,
+answers: 5,
+views: 100,
+createdAt : new Date()
+},
+{ _id : "2", title :"I want to learn Javascript ", description : "How do i go about learning js ", tags: [
+    {_id : "1", name : "js"},
+    {_id : "2", name :"Javascript"}
+  ], author : { _id : "2", name: "Jane Doe"},
+upvotes : 20,
+answers: 10,
+views: 200,
+createdAt : new Date()}
+
+]
+
+interface SearchParams {
+  searchParams: Promise<{[key: string] : string}>
+}
+
+const Home = async ({searchParams }: SearchParams) => {
+  const {query = ""} = await searchParams;
+
+  const filteredQuestions = questions.filter((question)=>(
+    query?.toLowerCase() === "" ||
+    question.title.toLowerCase().includes(query?.toLowerCase()) ||
+    question.description.toLowerCase().includes(query?.toLowerCase()) ||
+    question.tags.some((tag) => tag.name.toLowerCase().includes(query?.toLowerCase()))
+  ))
+  
   return (
     <>
       <section className=" flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center ">
@@ -16,14 +51,14 @@ const Home = async () => {
           <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
         </Button>
       </section>
-      <section className="mt-12">Local Search</section>
-      home filter
+      <section className="mt-12">
+        <LocalSearch imgSrc="/icons/search.svg" placeholder="Search questions..." otherClasses="flex-1" route="/" />
+      </section>
+     
       <div className=" mt-10 flex w-full flex-col gap-6">
-        <p>Question 1</p>
-        <p>Question 1</p>
-        <p>Question 1</p>
-        <p>Question 1</p>
-        <p>Question 1</p>
+        { filteredQuestions.map((question)=>(
+          <p key={question._id}>{question.title}</p>
+        ))}
       </div>
     </>
   );
---
+++ lib/url.ts
@@ -0,0 +1,43 @@
+import qs from "query-string"
+
+interface UrlQueryParams{
+    params: string;
+    key: string;
+    value: string;
+}
+
+interface removeKeyFromQueryParams {
+    params: string;
+    keysToRemove: string[];
+}
+
+export const formUrlQuery = ({params , key , value}: UrlQueryParams) => {
+    // change the params to object
+    const paramsString = qs.parse(params);
+
+    // appending if not already there or changing the value if already in the search params of the url
+    paramsString[key] = value;
+    
+    // converting back to string and returning
+    return qs.stringifyUrl({
+        url: window.location.href,
+        query: paramsString,
+    })
+}
+
+export const removeKeyFromQuery = ({params , keysToRemove } : removeKeyFromQueryParams) =>{
+     // change the params to object
+     const paramsString = qs.parse(params);
+
+     keysToRemove.forEach((key)=>{
+        delete paramsString[key];
+     })
+     
+     // converting back to string and returning
+     return qs.stringifyUrl({
+         url: window.location.href,
+         query: paramsString,
+     }, {
+        skipNull: true
+     })
+}
