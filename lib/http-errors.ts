5130f85fbcc9285f103101c98fdfb80fb176022a
s-kvng <kvngnathan8420@gmail.com>
2024-12-20 13:39:23 +0000

    Create http-errors.ts
    A class based file

---
+++ lib/http-errors.ts
@@ -0,0 +1,55 @@
+export class RequestError extends Error {
+    statusCode: number;
+    errors: Record<string, string[]>;
+
+    constructor(statusCode: number, message: string, errors: Record<string, string[]>) {
+        super(message);
+        this.statusCode = statusCode;
+        this.errors = errors;
+        this.name = "RequestError";
+    }
+}
+
+export class ValidationError extends RequestError {
+    constructor(fieldErrors: Record<string, string[]>){
+        const message = ValidationError.formatFieldError(fieldErrors)
+        super(400, message, fieldErrors);
+        this.name= "ValidationError"
+        this.errors = fieldErrors;
+    }
+
+    static formatFieldError(errors: Record<string, string[]>) {
+        const formattedMessage = Object.entries(errors).map(( [field , messages])=>{
+            const fieldErrors = field.charAt(0).toUpperCase() + field.slice(1);
+
+            if(messages[0] === "Required"){
+                return `${fieldErrors} is required`;    
+            }else{
+                return `${messages.join(' and ')}`;
+            }
+            
+        })
+
+    }
+};
+
+export class NotFoundError extends RequestError {
+    constructor(resourse: string){
+        super(404, `${resourse} not found`);
+        this.name = "NotFoundError"
+    }
+};
+
+export class ForbiddenError extends RequestError {
+    constructor(message: string = "Forbidden"){
+        super(403, message);
+        this.name = "ForbiddenError"
+    }
+};
+
+export class UnauthorizedError extends RequestError {
+    constructor(message: string = "Unauthorized"){
+        super(401, message);
+        this.name = "UnauthorizedError"
+    }
+};
