faab68f65c726f0b9daa8691ae86490614bbfe80
s-kvng <kvngnathan8420@gmail.com>
2025-04-04 00:37:39 +0000

    add account table schema

---
+++ database/account.model.ts
@@ -0,0 +1,22 @@
+import { model , Schema , models, Types } from 'mongoose';
+
+export interface IAccount {
+    userId : Types.ObjectId ;
+    accountType : string ;
+    providerAccountId : string ;
+    name : string ;
+    profileImage : string ;
+}
+
+export const AccountSchema = new Schema({
+    userId : { type : Schema.Types.ObjectId , ref : 'User' , required: true },
+    provider : { type : String , required: true },
+    providerAccountId : { type : String , required: true },
+    name : { type : String , required: true },
+    profileImage : { type : String  },
+    password: { type : String },
+}, {timestamps: true})
+
+// if the model is already created, use it. Otherwise create a new model
+const Account = models?.Account || model<IAccount>('Account', AccountSchema);
+export default Account;
